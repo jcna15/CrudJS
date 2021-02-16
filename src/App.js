6394326baef7b9018ac83ce50109e710c851d1f0
Nix <jcna15@gmail.com>
2021-02-16 15:29:42 -0500

    Implementing Delete and Modify Method

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, {useState} from 'react'
-import {isEmpty} from 'lodash'
+import {isEmpty,size} from 'lodash'
 import shortid from 'shortid'
 
 function App() {
   const [task, setTask] = useState("")
   const [tasks,setTasks] = useState([])
+  const [editMode,setEditMode] = useState(false)
+  const [id, setId] = useState("")
 
   const addTask =(e) =>{
     e.preventDefault()
@@ -19,6 +21,34 @@ function App() {
     }
 
     setTasks([...tasks,newTask])
+    setTask("")
+  }
+
+  const saveTask =(e) =>{
+    e.preventDefault()
+    if(isEmpty(task)){
+      console.log("Task Empty")
+      return
+    }
+
+    //setTasks([...tasks,newTask])
+    const editedTasks = tasks.map(item => item.id === id ? {id, name: task}: item)
+    setTasks(editedTasks)
+    setEditMode(false)
+    setTask("")
+    setId("")
+  }
+
+  const deleteTask = (id) =>{
+    const filteredTask = tasks.filter(task => task.id !== id)
+    setTasks(filteredTask)
+    setTask("")
+  }
+
+  const editTask = (theTask) =>{
+    setTask(theTask.name)
+    setEditMode(true)
+    setId(theTask.id)
   }
 
   return ( 
@@ -28,28 +58,38 @@ function App() {
       <div className="row">
         <div className="col-8"> 
           <h4 className="text-center">Lista de tareas</h4>
-          <ul className="list-group">
-
-            {            
+          {    
+          size(tasks) === 0 ? (
+            <h5 className="text-center">Aun no hay tareas programadas..</h5>
+          ):(
+            <ul className="list-group">
+            {
             tasks.map((task) => (
             <li className="list-group-item" key={task.id}>
               <span className="lead">{task.name}</span>
-              <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
-              <button className="btn btn-warning btn-sm float-right">Editar</button>
+              <button 
+                className="btn btn-danger btn-sm float-right mx-2"
+                onClick={() => deleteTask(task.id)}>Eliminar</button>
+              <button 
+                className="btn btn-warning btn-sm float-right"
+                onClick={() => editTask(task)}
+                >Editar</button>
             </li>
             ))
             }
-
           </ul>
+          )      
+          }
 
         </div>
         <div className="col-4"> 
-          <h4 className="text-center">Formulario</h4>
-            <form onSubmit={addTask}>
+          <h4 className="text-center">{editMode ? "Modificar Tarea": "Agregar Tareas"}</h4>
+            <form onSubmit={editMode ? saveTask:addTask}>
               <input type="text" className="form-control mb-2" placeholder="Ingrese la Tarea"
                 onChange={(text)=>setTask(text.target.value)}
                 value={task}/>
-              <button className="btn btn-dark btn-block" type="submit">Agregar</button>
+              <button className={editMode ? "btn btn-warning btn-block" : "btn btn-dark btn-block"} 
+                      type="submit">{editMode ? "Guardar":"Agregar"}</button>
             </form>
         </div>
 
